26dd222bb5c8f2e7841c3d02edd2ce2b1cf2953f
beaniejoy <lhb1025@gmail.com>
2021-02-20 02:37:41 +0900

    210220 첫번째 커밋

--- stack/Baek_1874.java
+++ stack/Baek_1874.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 import java.util.Stack;
 
 // https://www.acmicpc.net/problem/1874
-// Stack
 public class Baek_1874 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
