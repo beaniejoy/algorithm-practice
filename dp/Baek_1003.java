33df919c4604e7a8bfa8d7f8a1b791759065f898
beaniejoy <lhb1025@gmail.com>
2021-07-24 19:55:26 +0900

    update : 백준 문제 추가

--- dp/Baek_1003.java
+++ dp/Baek_1003.java
@@ -1,43 +1,55 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Baek_1003 {
-  public static void solve(int T, int[] questions) {
-    int max = 0;
-    // 입력 값중 가장 큰 수 지정
-    for (int i = 0; i < T; i++) {
-      if (max < questions[i])
-        max = questions[i];
+    public static int[][] solve(int T, int[] questions) {
+        int max = 0;
+        for(int i = 0; i < T; i++) {
+            if(max < questions[i]) max = questions[i];
+        }
+        
+        int[][] fibo = new int[max+1][2];
+        
+        fibo[0][0] = 1;
+        fibo[0][1] = 0;
+        fibo[1][0] = 0;
+        fibo[1][1] = 1;
+        
+        for(int i = 2; i <= max; i++) {
+            fibo[i][0] = fibo[i-1][0] + fibo[i-2][0];
+            fibo[i][1] = fibo[i-1][1] + fibo[i-2][1];
+        }
+        
+        int[][] answers = new int[T][2];
+        for(int i = 0; i < T; i++) {
+            answers[i][0] = fibo[questions[i]][0];
+            answers[i][1] = fibo[questions[i]][1];
+        }
+        
+        return answers;
     }
-    // 가장 큰 수를 기준으로 Array생성
-    int[] fibo = new int[max + 1];
-    fibo[0] = 0;
-    fibo[1] = 1;
-
-    for (int i = 2; i <= max; i++) {
-      fibo[i] = fibo[i - 1] + fibo[i - 2];
+    
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int T = Integer.parseInt(br.readLine());
+        
+        int[] questions = new int[T];
+        
+        for(int i = 0; i < T; i++) {
+            questions[i] = Integer.parseInt(br.readLine());
+        }
+        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        for(int[] answer : solve(T, questions)) {
+        	bw.write(answer[0] + " " + answer[1] + "\n");
+        }
+        bw.flush();
+        
+        bw.close();
+        br.close();
     }
-
-    for (int i = 0; i < T; i++) {
-      if (questions[i] == 0)
-        System.out.println("1 0");
-      else
-        System.out.println(fibo[questions[i] - 1] + " " + fibo[questions[i]]);
-    }
-
-  }
-
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt();
-
-    int[] questions = new int[T];
-
-    for (int i = 0; i < T; i++) {
-      questions[i] = sc.nextInt();
-    }
-
-    solve(T, questions);
-
-    sc.close();
-  }
-}
+} 
--- dp/Baek_11726.java
+++ dp/Baek_11726.java
@@ -1,23 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class Beak_11726 {
-
-  public static void solve(int N) {
-    int[] arr = new int[N + 1];
-    arr[0] = 1;
-    arr[1] = 1;
-    for (int i = 2; i <= N; i++) {
-      // N이 1000이 넘어가면 원치않은 결과값 출력
-      // 배열에 넣을 때 10007 나눈 나머지 입력
-      arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
-    }
-    System.out.print(arr[N]);
-  }
-
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    solve(N);
-    sc.close();
-  }
+public class Baek_11726 {
+	
+	public static int solve(int N) {
+		int[] arr = new int[N+1];
+		
+		arr[0] = 1;
+		if(N > 0) arr[1] = 1;
+		
+		
+		for(int i = 2; i <= N; i++) {
+			arr[i] = (arr[i-1] + arr[i-2]) % 10007;
+		}
+		
+		return arr[N];
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(String.valueOf(solve(N)));
+		bw.flush();
+		
+		bw.close();
+		br.close();
+	}
 }
