fc2eba22dfd6dc7d2d712e9edf2d23ae004d99bf
beaniejoy <lhb1025@gmail.com>
2021-08-13 00:51:06 +0900

    [백준1874] 배열로 문제풀기

--- stack/Baek_1874.java
+++ stack/Baek_1874.java
@@ -1,58 +1,79 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-// https://www.acmicpc.net/problem/1874
+/**
+ * Collection 말고 array로 풀어보기
+ * 비교를 위해 두 개의 배열 필요
+ * 	- 문제에 주어진 배열 
+ * 	- pop, push 대상이되는 배열
+ * 
+ * https://www.acmicpc.net/problem/1874
+ */
 public class Baek_1874 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-
-		int[] output = new int[N];
-		ArrayList<String> result = new ArrayList<>();
-		for (int i = 0; i < N; i++) {
-			output[i] = sc.nextInt();
-		}
-
-		sc.close();
-
-		Stack<Integer> stack = new Stack<>();
-		// output's index
-		int index = 0;
-		// count for pushing
-		int pushCount = 0;
-
-		while (pushCount < N) {
-			if (stack.isEmpty() || stack.peek() != output[index]) {
-				stack.push(++pushCount);
-				result.add("+");
-				continue;
-			}
-
-			while (true) {
-				stack.pop();
-				result.add("-");
+	public static char[] solve(int[] arr, int N) {
+		int curNum = 1;		// 1 ~ N 순차적으로 증가할 숫
+		int curIdx = 0;		// 비교대상 array의 index
+		int resultIdx = 0;	// 출력값을 담을 array의 index
+		int index = 0;		// 주어진 array의 index
+		
+		int[] input = new int[N];
+		char[] result = new char[N*2];
+		
+		while(true) {
+			// push
+			input[curIdx] = curNum;
+			result[resultIdx++] = '+';
+			
+			// pop (주어진 arr의 요소와 같은 값인 경우)
+			while(input[curIdx] == arr[index]) {
+				result[resultIdx++] = '-';
+				
 				index++;
-
-				if (stack.isEmpty() || stack.peek() != output[index])
-					break;
+				curIdx--;
+				
+				if (curIdx < 0) break;
 			}
-		}
-
-		while (index < N) {
-			if (stack.peek() != output[index]) {
-				System.out.println("NO");
-				return;
+			
+			if(index >= N) break;
+			if(curNum >= N) {
+				result[0] = 'N';
+				return result;
 			}
-
-			stack.pop();
-			result.add("-");
-			index++;
+			curIdx++;
+			curNum++;
 		}
+		
+		return result;
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-		for (String s : result) {
-			System.out.println(s);
-		}
+        int[] output = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+        	output[i] = Integer.parseInt(br.readLine());
+        }
+        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        char[] result = solve(output, N);
+
+        for(char c : result) {
+        	if(c == 'N') {
+        		bw.write("NO");
+        		break;
+        	}
+        	bw.write(c);
+        	bw.newLine();
+        }
+        
+        bw.flush();
+
+        bw.close();
+        br.close();
 	}
 }
