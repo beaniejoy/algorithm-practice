8d728fcb13d579f7233565131510f0a78caed74f
beaniejoy <lhb1025@gmail.com>
2021-08-14 03:13:53 +0900

    [이슈] 프린터 큐 - 우선순위큐 생성보다 배열 사용 - 우선순위큐와 배열 sorting 성능 차이 이슈

--- queue/Baek_1966.java
+++ queue/Baek_1966.java
@@ -1,72 +1,109 @@
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-// https://www.acmicpc.net/problem/1966
+/**
+ * https://www.acmicpc.net/problem/1966
+ * - issue
+ * 우선순위큐 객체 생성보다 배열객체 생성하는 것이 더 가볍다.
+ * 우선순위큐 내부적으로 max heap 사용해서 최대값 반환,
+ * 배열 sorting은 quick sort 사용하는 것으로 보임
+ * 이 두 개의 성능상 차이는 어느정도인지 알아봐야 할 듯
+ */
 public class Baek_1966 {
 
-  static class Node {
-    private int index;
-
-    private int order;
-
-    Node(int index, int order) {
-      this.index = index;
-      this.order = order;
+  private static class Node {
+    private boolean target;
+    private int importance;
+    
+    /**
+     * @param target 몇번째 프린트 순서인지 알고자하는 대상 여부
+     * @param importance 중요도
+     */
+    public Node(boolean target, int importance) {
+      this.target = target;
+      this.importance = importance;
+    }
+    
+    /**
+     * @return 원하는 대상인지 여부 반환
+     */
+    public boolean isTarget() {
+      return this.target;
     }
   }
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int numCases = sc.nextInt();
-    // print 대기 Q
-    Queue<Node> printQ = new LinkedList<>();
-    // 우선순위가 큰 순서부터 뽑기위한 Q
-    Queue<Integer> orderQ = new PriorityQueue<Integer>(Collections.reverseOrder());
-
-    // 결과를 넣기위한 array 생성
-    int[] result = new int[numCases];
-    int index = 0;
-
-    for (int i = 0; i < numCases; i++) {
-
-      int numDoc = sc.nextInt();
-      int found = sc.nextInt();
-
-      // number of Documents
-      for (int j = 0; j < numDoc; j++) {
-        int input = sc.nextInt();
-        printQ.offer(new Node(j, input));
-        orderQ.offer(input);
-      }
-
-      int order = 1;
-      while (!printQ.isEmpty()) {
-        Node tmp = printQ.poll();
-        // Q안에 남아있는 데이터들 중 가장 우선순위가 큰 작업인지 비교
-        if (tmp.order == orderQ.peek()) {
-          if (tmp.index == found) {
-            result[index++] = order;
-            break;
-          }
-
-          order++;
-          orderQ.poll();
-        } else {
-          printQ.offer(tmp);
+  public static int solve(Queue<Node> printQ, int[] importances) {
+    int answer = 1;
+    int idx = importances.length - 1;
+    
+    // 정렬
+    Arrays.sort(importances);
+    
+    while(!printQ.isEmpty()) {
+      Node node = printQ.poll();
+      if(importances[idx] == node.importance) {
+        if(node.isTarget()) {
+          return answer;
         }
+        
+        idx--;
+        answer++;
+      } else {
+        printQ.offer(node);
       }
-
-      // 초기화
-      printQ.clear();
-      orderQ.clear();
+      
     }
+    
+    return -1;
+  }
 
-    for (int p : result)
-      System.out.println(p);
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int totalCase = Integer.parseInt(br.readLine());
+    int[] answers = new int[totalCase];
+    
+    StringTokenizer st = null;
+    Queue<Node> printQ = new LinkedList<>();
+    
+    for(int i = 0; i < totalCase; i++) {
+    st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    
+    // 성능이 살짝 떨어진다. 
+    // 단순배열 생성하는 것보다 Collection 프레임워크 객체 생성하고 사용하는 것이 더 무겁다.
+    // PriorityQueue<Integer> importances = new PriorityQueue<>(Collections.reverseOrder());
+    int[] importances = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for(int j = 0; j < N; j++) {
+      boolean target = false;
+      int importance = Integer.parseInt(st.nextToken());
+      
+      if(j == M) target = true;
+      printQ.add(new Node(target, importance));
+      importances[j] = importance;
+    }
+      answers[i] = solve(printQ, importances);
+      printQ.clear();
+    }
+    
+    for(int answer : answers) {
+      bw.write(String.valueOf(answer));
+      bw.newLine();
+    }
+    
+    bw.flush();
 
-    sc.close();
+    bw.close();
+    br.close();
   }
 }
